890e14072482934cf79b05d9d441424834fae0f0
Daniel Tweed <m.danieltweed@gmail.com>
2016-09-30 11:37:36 -0400

    updated helper fcns

--- secctp.c
+++ secctp.c
@@ -2,12 +2,15 @@
 #include "secctp.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /* Helper function for SecCTP messages
  * Builds standard message from status lines
  * Returns 0 on success, -1 indicates some fields invalid
  * (Reference SecCTP draft Sec. 7 )
 */
+
 int generateMsg(char *msg, char *headers, char *body) {
 	int ret = -1;
 	if (msg && headers) {		
@@ -25,11 +28,22 @@ int generateMsg(char *msg, char *headers, char *body) {
  * Returns 0 on success, -1 indicates some fields invalid
  * (Reference SecCTP draft Sec. 7.1 )
 */
-int generateHello(char *msg, char *method, char *headers, char *body){
+int generateHello(char *msg, char *method, char *user_headers, char *body){
 	int ret = -1;
-	if (msg && method && headers) {
-		sprintf(msg, "%s %s\r\n", method, VERSION);
-		ret = generateMsg(msg, headers, body);
+	char *headers = (char*)malloc(MAX_HEADER_SIZE);
+//Hello messages require date and accepted languages header fields
+	if (headers) {			
+		time_t now = time(0);
+		struct tm tm = *gmtime(&now);
+   		strftime(headers,MAX_HEADER_SIZE,"Date: %a, %d %b %Y %H:%M:%S %Z\r\n", &tm);
+		strcat(headers, lang);
+		if (user_headers) {
+			strcat(headers,user_headers);  //Not robust against overflow yet
+		} 	
+		if (msg && method) {
+			sprintf(msg, "%s %s\r\n", method, VERSION);
+			ret = generateMsg(msg, headers, body);
+		}
 	}
 	return ret;
 }
