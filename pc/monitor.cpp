43046f253ec8a5f76a78f3fb76bc6cd9cc4f204e
Daniel Tweed <m.danieltweed@gmail.com>
2016-10-20 13:34:24 -0400

    pc/monitor done until test with secnic

--- pc/monitor.cpp
+++ pc/monitor.cpp
@@ -4,6 +4,16 @@
 #include <boost/regex.hpp>
 #include "tins/tcp_ip/stream_follower.h"
 #include "tins/sniffer.h"
+#include "tins/ip_address.h"
+
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
 using std::string;
 using std::cout;
@@ -19,88 +29,60 @@ using Tins::Sniffer;
 using Tins::SnifferConfiguration;
 using Tins::TCPIP::Stream;
 using Tins::TCPIP::StreamFollower;
+using Tins::IPv4Address;
 
+
+#define PROCESSING 302  //102 when not testing
 
 // Don't buffer more than 3kb of data in either request/response
 const size_t MAX_PAYLOAD = 3 * 1024;
-// The regex to be applied on the request. This will extract the HTTP
-// method being used, the request's path and the Host header value.
-regex request_regex("([\\w]+) ([^ ]+).+\r\nHost: ([\\d\\w\\.-]+)\r\n");
-// The regex to be applied on the response. This finds the response code.
-regex response_regex("HTTP/[^ ]+ ([\\d]+)");
 
-void on_server_data(Stream& stream) {
-    match_results<Stream::payload_type::const_iterator> client_match;
-    match_results<Stream::payload_type::const_iterator> server_match;
-    const Stream::payload_type& client_payload = stream.client_payload();
-    const Stream::payload_type& server_payload = stream.server_payload();
-    // Run the regexes on client/server payloads
-    bool valid = regex_search(server_payload.begin(), server_payload.end(),
-                              server_match, response_regex) &&
-                 regex_search(client_payload.begin(), client_payload.end(),
-                              client_match, request_regex);
-    // If we matched both the client and the server regexes
-    if (valid) {
-        // Extract all fields
-        string method = string(client_match[1].first, client_match[1].second);
-        string url = string(client_match[2].first, client_match[2].second);
-        string host = string(client_match[3].first, client_match[3].second);
-        string response_code = string(server_match[1].first, server_match[1].second);
-        // Now print them
-        cout << method << " http://" << host << url << " -> " << response_code << endl;
-	
-        // Once we've seen the first request on this stream, ignore it
-        stream.ignore_client_data();
-        stream.ignore_server_data();
-    }
-    
-    // Just in case the server returns invalid data, stop at 3kb
-    if (stream.server_payload().size() > MAX_PAYLOAD) {
-        stream.ignore_server_data();
-    }
-}
+//Some regex to extract necessary data
+ //Get the message code
+regex code_regex("HTTP/[^ ]+ ([\\d]+)");
+ //Search for SecCTP URI (assume hostname is same as main server, extract from active stream)
+//regex uri_regex("SecCTP-URI: ((?:\"[^\"\n]*\"|[^\r\n])*)");  //format hostname:port per RFC 3986
 
-void on_client_data(Stream& stream) {
-    // Don't hold more than 3kb of data from the client's flow
-    if (stream.client_payload().size() > MAX_PAYLOAD) {
-        stream.ignore_client_data();
-    }
-}
+//For testing and later, extracting all http headers
+regex headers_regex("((?:\"[^\"\n]*\"|[^:,\n])*):((?:\"[^\"\n]*\"|[^,\n])*)");
 
-void on_new_connection(Stream& stream) {
-    stream.client_data_callback(&on_client_data);
-    stream.server_data_callback(&on_server_data);
-    // Don't automatically cleanup the stream's data, as we'll manage
-    // the buffer ourselves and let it grow until we see a full request
-    // and response
-    stream.auto_cleanup_payloads(false);
-}
+
+int sockfd, portno, n;
+struct sockaddr_in serv_addr;
+struct hostent *server;
+
+void on_server_data(Stream& stream);
+void on_client_data(Stream& stream);
+void on_new_connection(Stream& stream);
+int signalNIC(string server_addr, string secCTP_uri);
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        cout << "Usage: " << argv[0] << " <interface>" << endl;
+    if (argc != 4) {
+        cout << "Usage: " << argv[0] << " <interface> <nic hostname/IP> <nic port>" << endl;
         return 1;
-    }
-
-    try {
-        // Construct the sniffer configuration object
-        SnifferConfiguration config;
-        // Only capture TCP traffic sent from/to port 80
-        config.set_filter("tcp port 80");
-        // Construct the sniffer we'll use
+    } try {		
+		//A few extractions for the connection to the SecNIC	    
+		portno = atoi(argv[3]);		
+		if ( (server = gethostbyname(argv[2])) == NULL) {
+			cerr << "ERROR invalid server" << endl;
+			return 1;
+		}
+		bzero((char *) &serv_addr, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,server->h_length);
+		serv_addr.sin_port = htons(portno);
+		
+				
+        // Config and construct sniffer
+        SnifferConfiguration config;        
+        config.set_filter("tcp port 80"); //Only intersted in http         
         Sniffer sniffer(argv[1], config);
 
         cout << "Starting capture on interface " << argv[1] << endl;
 
-        // Now construct the stream follower
-        StreamFollower follower;
-        // We just need to specify the callback to be executed when a new 
-        // stream is captured. In this stream, you should define which callbacks
-        // will be executed whenever new data is sent on that stream 
-        // (see on_new_connection)
+        // Construct and set up stream follower
+        StreamFollower follower;        
         follower.new_stream_callback(&on_new_connection);
-        // Now start capturing. Every time there's a new packet, call 
-        // follower.process_packet
         sniffer.sniff_loop([&](Packet& packet) {
             follower.process_packet(packet);
             return true;
@@ -111,3 +93,74 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 }
+
+int signalNIC(string server_addr, string uri) {
+	int wc = -1;
+	string msg;
+	string delim = ":";
+	std::ostringstream oss; 
+	
+	if ( (sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+			cerr << "ERROR opening socket" << endl;
+			return 1;
+	}
+	
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+		cerr << "ERROR connecting to server " << endl;
+		
+	} else {
+		//Build msg		
+		oss << "Stream-Address:" << server_addr << "\n";
+		oss << "SecCTP-Address:" << uri.substr(0,uri.find(delim)) << "\n";
+		oss << "SecCTP-Port:" << uri.substr(uri.find(delim)+1) << "\n";		
+		msg = oss.str();		
+		
+		//Send msg
+		wc = write(sockfd, msg.data(), msg.length());		
+		
+			
+		cout << "Success" << endl;
+	}	
+	close(sockfd);	
+	return wc;
+}
+
+void on_server_data(Stream& stream) {
+    match_results<Stream::payload_type::const_iterator> server_match;
+    match_results<Stream::payload_type::const_iterator> client_match;
+    
+    const Stream::payload_type& server_payload = stream.server_payload();
+    
+    // Run the regex on server payload
+    bool valid = regex_search(server_payload.begin(), server_payload.end(),
+                              server_match, code_regex) && 
+                 regex_search(server_payload.begin(), server_payload.end(),
+							client_match, headers_regex);//uri_regex);
+	
+    if (valid) {   		
+        string response_code = string(server_match[1].first, server_match[1].second);        		
+		string secCTP_uri = string(client_match[1].first, client_match[1].second);
+
+		if (std::stoi(response_code) == PROCESSING) {
+			IPv4Address server_addr = stream.server_addr_v4();
+			cout << server_addr.to_string() << endl;			
+			
+			if (signalNIC(server_addr.to_string(), secCTP_uri) < 0)
+				cerr << "Error in signaling SecNIC for auth" << endl;
+		}
+    }
+    
+    // Just in case the server returns invalid data, stop at 3kb
+    if (stream.server_payload().size() > MAX_PAYLOAD) {
+        stream.ignore_server_data();
+    }
+}
+
+void on_new_connection(Stream& stream) {
+	
+    stream.ignore_client_data(); //Only monitoring server messages for now
+    stream.server_data_callback(&on_server_data);	
+    stream.auto_cleanup_payloads(true); //No need to buffer the data
+}
+
+
